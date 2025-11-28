7dbb78c5af8c068edf5fd8b0120f66df16cfa065
bmichael <bmichael03@vt.edu>
2025-11-28 17:24:18 -0500

    updated pyproject.toml

--- src/napari_piscis/_version.py
+++ src/napari_piscis/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.4.dev0+gfbd7a9a23.d20251128'
-__version_tuple__ = version_tuple = (0, 1, 4, 'dev0', 'gfbd7a9a23.d20251128')
+__version__ = version = '0.1.4.dev1+gc1524f519.d20251128'
+__version_tuple__ = version_tuple = (0, 1, 4, 'dev1', 'gc1524f519.d20251128')
 
-__commit_id__ = commit_id = 'gfbd7a9a23'
+__commit_id__ = commit_id = 'gc1524f519'
