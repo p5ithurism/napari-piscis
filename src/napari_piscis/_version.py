a1aacb63af90fd63a6f94e5574f3370499e18729
bmichael <bmichael03@vt.edu>
2025-11-30 17:29:50 -0500

    dependency fixes for python 3.11

--- src/napari_piscis/_version.py
+++ src/napari_piscis/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.19.post1.dev0'
-__version_tuple__ = version_tuple = (0, 1, 19, 'post1', 'dev0')
+__version__ = version = '0.1.19.post1.dev1'
+__version_tuple__ = version_tuple = (0, 1, 19, 'post1', 'dev1')
 
-__commit_id__ = commit_id = 'g3ef92dc73'
+__commit_id__ = commit_id = 'g1d353f753'
