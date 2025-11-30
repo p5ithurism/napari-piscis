e6f3b04944b789f23c85966b0e7f2f470eee8b39
bmichael <bmichael03@vt.edu>
2025-11-30 18:33:46 -0500

    removed 3.13 support (jax .get_backend is deprecated)

--- src/napari_piscis/_version.py
+++ src/napari_piscis/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.19.post1.dev1'
-__version_tuple__ = version_tuple = (0, 1, 19, 'post1', 'dev1')
+__version__ = version = '0.1.19.post1.dev2'
+__version_tuple__ = version_tuple = (0, 1, 19, 'post1', 'dev2')
 
-__commit_id__ = commit_id = 'g1d353f753'
+__commit_id__ = commit_id = 'ga1aacb63a'
