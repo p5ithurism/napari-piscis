f25c8bdc6b359f496fb452824c1b918aa074840c
bmichael <bmichael03@vt.edu>
2025-12-01 12:32:20 -0500

    improved code coverage

--- src/napari_piscis/_version.py
+++ src/napari_piscis/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.20.post1.dev4'
-__version_tuple__ = version_tuple = (0, 1, 20, 'post1', 'dev4')
+__version__ = version = '0.1.20.post1.dev5'
+__version_tuple__ = version_tuple = (0, 1, 20, 'post1', 'dev5')
 
-__commit_id__ = commit_id = 'g6a26347d3'
+__commit_id__ = commit_id = 'gcda67e9a0'
